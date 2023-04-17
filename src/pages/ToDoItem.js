118da3b3c8c2fb97d6ee3580afe35775f0774b5d
Michalis Drakoulakos <mikerx87@gmail.com>
2023-04-17 16:39:28 +0300

    favicon

---
+++ src/pages/ToDoItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function ToDoItem(props) {
+  const [isDone, setIsDone] = useState(false);
+
+  function handleClick() {
+    setIsDone((prevItem) => {
+      return !prevItem;
+    });
+  }
+
+  return (
+    <div onClick={handleClick}>
+      <li
+        style={{
+          textDecoration: isDone ? "line-through" : "none",
+          cursor: "pointer"
+        }}
+      >
+        {props.text}
+      </li>
+    </div>
+  );
+}
+
+export default ToDoItem;
